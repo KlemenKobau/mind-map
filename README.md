486b720366f0315bbf5a0c864be051bfe36a3578
Klemen Kobau <klemen.kobau@gmail.com>
2026-03-29 14:21:17 +0200

    feat: added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mind map
+
+## TODO
+
+- create a chat bot that asks the user what he would like to analyze.
+- the user should input the string
+- The bot should create a graph node, representing the field of interest the user imputed,
+along with connections to other relevant subjects
+
+## MVP
+- [ ] Create a graph representation
+- [ ] Ask the user for the search topic
+- [ ] Create a node, based on user instruction
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use anyhow::Result;
 
-use ollama_rs::{Ollama, generation::completion::request::GenerationRequest};
+use ollama_rs::generation::chat::ChatMessage;
+use ollama_rs::generation::tools::implementations::{Calculator, DDGSearcher, Scraper};
+use ollama_rs::{
+    Ollama, coordinator::Coordinator, generation::completion::request::GenerationRequest,
+    models::ModelOptions,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let ollama = Ollama::default();
 
-    let req = GenerationRequest::new("qwen3.5:4b".to_string(), "Why is the sky blue?").think(false);
-    let res = ollama.generate(req).await?;
+    let history = vec![];
 
-    println!("{}", res.response);
+    let mut coordinator = Coordinator::new(ollama, "qwen3.5:4b".to_string(), history)
+        .options(ModelOptions::default())
+        .add_tool(Scraper {})
+        .add_tool(DDGSearcher::new())
+        .add_tool(Calculator {});
+
+    let resp = coordinator
+        .think(false)
+        .chat(vec![ChatMessage::user(
+            "What do you know about destiny the political streamer, search online".to_string(),
+        )])
+        .await
+        .unwrap();
+
+    println!("{}", resp.message.content);
 
     Ok(())
 }
