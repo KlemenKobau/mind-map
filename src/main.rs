1c588f6ed128331b4836722d9e3fd08eb7bcfd16
Klemen Kobau <klemen.kobau@gmail.com>
2026-03-31 20:01:58 +0200

    feat: changed model

--- src/main.rs
+++ src/main.rs
@@ -16,35 +16,11 @@ mod graph;
 async fn main() -> Result<()> {
     let ollama = Ollama::default();
 
-    /*
-
-    let history = vec![];
-    let format: FormatType = FormatType::StructuredJson(Box::new(JsonStructure::new::<NodeData>()));
-
-
-    let mut coordinator = Coordinator::new(ollama, "qwen3.5:4b".to_string(), history)
-        .options(ModelOptions::default())
-        .add_tool(Scraper {})
-        .add_tool(DDGSearcher::new())
-        .add_tool(Calculator {})
-        .format(format);
-
-    let resp = coordinator
-        .think(false)
-        .chat(vec![ChatMessage::user(
-            "I would like to learn more about cars".to_string(),
-        )])
-        .await
-        .unwrap();
-
-    println!("{}", resp.message.content);
-    */
-
     let format = FormatType::StructuredJson(Box::new(JsonStructure::new::<NodeData>()));
     dbg!(&format);
     let res = ollama
         .generate(
-            GenerationRequest::new("qwen2.5:3b".to_string(), "Tell me more about cars")
+            GenerationRequest::new("qwen2.5:7b".to_string(), "Tell me more about cars")
                 .format(format)
                 .think(false),
         )
