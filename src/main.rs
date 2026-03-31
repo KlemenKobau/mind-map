b935c008ccd427b2ce80228837c16ac328e54d03
Klemen Kobau <klemen.kobau@gmail.com>
2026-03-31 20:27:15 +0200

    fix: disable WebKit DMA-BUF renderer to fix GBM buffer error on Linux

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,7 @@ mod ollama;
 fn main() {
     // Must be set before GTK/GDK initializes (Wayland compositor incompatibility on Linux)
     unsafe { std::env::set_var("GDK_BACKEND", "x11") };
+    // Disable WebKit DMA-BUF renderer (GBM buffer creation fails on some Linux GPU drivers)
+    unsafe { std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1") };
     dioxus::launch(app::App);
 }
