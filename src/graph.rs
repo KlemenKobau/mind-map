f5a1922d557a0435f49735913522a386684c811e
Klemen Kobau <klemen.kobau@gmail.com>
2026-03-31 20:20:46 +0200

    feat: add Dioxus desktop dependency and extend NodeData with related concepts

--- src/graph.rs
+++ src/graph.rs
@@ -1,27 +1,9 @@
 use ollama_rs::generation::parameters::JsonSchema;
-use petgraph::graph::{Node, UnGraph};
 use serde::Deserialize;
 
-#[derive(JsonSchema, Deserialize, Debug)]
+#[derive(JsonSchema, Deserialize, Debug, Clone)]
 pub struct NodeData {
     pub name: String,
     pub description: String,
-}
-
-#[derive(Debug)]
-pub struct EdgeData {}
-
-#[derive(Debug)]
-pub struct GraphManager {
-    graph: UnGraph<NodeData, EdgeData>,
-}
-
-impl GraphManager {
-    pub fn add_node(&mut self, node_data: NodeData) {
-        self.graph.add_node(node_data);
-    }
-
-    pub fn get_all_nodes(&self) -> &[Node<NodeData>] {
-        self.graph.raw_nodes()
-    }
+    pub related: Vec<String>,
 }
