4831f284ffac6cd5ce2a1db65094d3cd2ff51f1d
Klemen Kobau <klemen.kobau@gmail.com>
2026-04-01 19:07:48 +0200

    chore: more comments

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ mod ollama;
 
 fn main() {
     // Must be set before GTK/GDK initializes (Wayland compositor incompatibility on Linux)
+    // needed to be able to resize the window
     unsafe { std::env::set_var("GDK_BACKEND", "x11") };
     // Disable WebKit DMA-BUF renderer (GBM buffer creation fails on some Linux GPU drivers)
+    // another workaround for wayland
     unsafe { std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1") };
     dioxus::launch(app::App);
 }
